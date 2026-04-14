1b6b98e840509fbe54480de3dadac2dd911ef10c
Alex Turner <alexturner@meta.com>
2026-04-14 16:31:52 -0700

    Fix BCOffset namespace: jit::BCOffset{bc_off}

--- Python/jit/hir/hir_c_api.cpp
+++ Python/jit/hir/hir_c_api.cpp
@@ -210,7 +210,7 @@ HirInstr hir_block_append(HirBasicBlock block, HirInstr instr) {
 }
 
 HirInstr hir_block_append_at(HirBasicBlock block, HirInstr instr, int32_t bc_off) {
-  as_instr(instr)->setBytecodeOffset(BCOffset{bc_off});
+  as_instr(instr)->setBytecodeOffset(jit::BCOffset{bc_off});
   return as_block(block)->Append(as_instr(instr));
 }
 
