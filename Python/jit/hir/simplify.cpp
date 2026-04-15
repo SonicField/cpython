201a63dcdeb0bd906da0737d18aad9720629ac85
Alex Turner <alexturner@meta.com>
2026-04-14 17:02:27 -0700

    Fix missing hir_instr_c.h include in simplify.cpp for LoadConst factory

--- Python/jit/hir/simplify.cpp
+++ Python/jit/hir/simplify.cpp
@@ -4,6 +4,7 @@
 #include "cinderx/Jit/jit_config_c.h"
 #include "cinderx/Jit/hir/hir_type_c.h"
 #include "cinderx/Jit/hir/hir_c_api.h"
+#include "cinderx/Jit/hir/hir_instr_c.h"
 
 #include "pycore_long.h"
 
