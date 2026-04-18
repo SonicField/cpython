806da6430483c46396c5bfc47d86a7212d95ff38
Alex Turner <alexturner@meta.com>
2026-04-18 12:30:12 -0700

    Revert "refcount_insertion: fix second hir_liveness_is_live_in call site"
    This reverts commit a27ab77c46371c2cdf5b4b2725443f3cdf1d60cd.

--- Python/jit/hir/refcount_pass_c.c
+++ Python/jit/hir/refcount_pass_c.c
@@ -464,7 +464,7 @@ void phx_rc_initialize_in_state(
 
         for (int ci = 0, cn = phx_rs_num_copies(pred_rs); ci < cn; ci++) {
             void *copy = phx_rs_copy(pred_rs, ci);
-            if (phx_rc_liveness_is_live_in(env->func, block, copy)) {
+            if (hir_liveness_is_live_in(env->liveness_state, block, copy)) {
                 phx_rs_add_copy(rs, copy);
             }
         }
