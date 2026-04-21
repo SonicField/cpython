01392198508fcae9bd9e754d2506411f5608f671
Alex Turner <alexturner@meta.com>
2026-04-20 18:19:00 -0700

    dead code: delete DataflowAnalysis hierarchy + helpers (-349 lines)
    Delete entire C++ dataflow infrastructure from analysis.cpp/h:
    - DataflowAnalysis base class (6 methods, 80 lines)
    - BackwardDataflowAnalysis::Run (28 lines)
    - ForwardDataflowAnalysis::Run (27 lines)
    - analyzeInstrLiveness template (38 lines)
    - kEmptyRegSet global + operator<<(RegisterSet) (15 lines)
    - Dead includes: bitvector.h, dataflow.h, fmt, iosfwd, printer.h
    
    All replaced by C equivalents: liveness_c.c, assignment_c.c,
    dominator_c.c. Only registerTypeMatches + operandsMustMatch remain
    (still used by hir_c_api.cpp). analysis.cpp 520→79 lines this session.

--- Python/jit/hir/analysis.h
+++ Python/jit/hir/analysis.h
@@ -2,13 +2,9 @@
 
 #pragma once
 
-#include "cinderx/Jit/bitvector.h"
-#include "cinderx/Jit/dataflow.h"
 #include "cinderx/Jit/hir/function.h"
 #include "cinderx/Jit/hir/hir.h"
 
-#include <iosfwd>
-#include <unordered_map>
 #include <unordered_set>
 
 namespace jit::hir {
@@ -17,124 +13,8 @@ class BasicBlock;
 class Register;
 
 using RegisterSet = std::unordered_set<Register*>;
-extern const RegisterSet kEmptyRegSet;
 
-std::ostream& operator<<(std::ostream& os, const RegisterSet& set);
-
-// Returns true iff the constraint signifies that all of its instruction's
-// operands must match
 bool operandsMustMatch(OperandType op_type);
-
-// Returns true if the type satisfies the passed in OperandType
 bool registerTypeMatches(Type op_type, OperandType expected_type);
 
-// Base class for dataflow analyses that compute facts about registers in the
-// HIR.
-//
-// An analysis will typically inherit from either ForwardDataflowAnalysis or
-// BackwardDataflowAnalysis and provide implementations of `ComputeGenKill`,
-// `ComputeNewIn`, and `ComputeNewOut` that implement the analysis.
-//
-// This is potentially a good candidate for CRTP if the performance overhead of
-// the virtual functions becomes an issue.
-class DataflowAnalysis {
- public:
-  explicit DataflowAnalysis(const Function& irfunc)
-      : irfunc_(irfunc),
-        num_bits_(0),
-        df_analyzer_(),
-        df_entry_(),
-        df_exit_(),
-        df_blocks_() {}
-
-  virtual ~DataflowAnalysis() {}
-
-  virtual void Run() = 0;
-
-  RegisterSet GetIn(const BasicBlock* block);
-  RegisterSet GetOut(const BasicBlock* block);
-
- protected:
-  virtual void ComputeGenKill(
-      const BasicBlock* block,
-      RegisterSet& gen,
-      RegisterSet& kill) = 0;
-  virtual jit::util::BitVector ComputeNewIn(
-      const jit::optimizer::DataFlowBlock* block) = 0;
-  virtual jit::util::BitVector ComputeNewOut(
-      const jit::optimizer::DataFlowBlock* block) = 0;
-
-  // Should be overridden by subclasses to set an appropriate uninitialized in-
-  // or out-state on the given block, if it should be something other than all
-  // zeros.
-  virtual void setUninitialized(jit::optimizer::DataFlowBlock* block) = 0;
-
-  virtual void Initialize();
-  void AddBasicBlock(const BasicBlock* cfg_block);
-
-  virtual std::string name() = 0;
-
-  void dump();
-
-  const Function& irfunc_;
-  size_t num_bits_;
-  jit::optimizer::DataFlowAnalyzer<Register*> df_analyzer_;
-  jit::optimizer::DataFlowBlock df_entry_;
-  jit::optimizer::DataFlowBlock df_exit_;
-  std::unordered_map<const BasicBlock*, jit::optimizer::DataFlowBlock>
-      df_blocks_;
-};
-
-class BackwardDataflowAnalysis : public DataflowAnalysis {
- public:
-  explicit BackwardDataflowAnalysis(const Function& irfunc)
-      : DataflowAnalysis(irfunc) {}
-
-  void Run() override;
-};
-
-class ForwardDataflowAnalysis : public DataflowAnalysis {
- public:
-  explicit ForwardDataflowAnalysis(const Function& irfunc)
-      : DataflowAnalysis(irfunc) {}
-
-  void Run() override;
-};
-
-
-// This computes which registers have been initialized at a basic block.
-//
-// A register is definitely assigned if it has been assigned to along all paths
-// into a block. A register is maybe assigned if has been assigned along any
-// path to the block.
-//
-// This information can be used to eliminate null checks for variables that are
-// definitely assigned.
-//
-// NB: This doesn't support DEL_FAST yet (and probably never will).
-//
-// We probably don't need to run this over temporaries. They should always be
-// assigned before being used.
-//
-// Each bit in the bit-vector represents whether or not the corresponding
-// register has been assigned. Local summaries for each block are computed as
-// follows:
-//
-//
-//   foreach instruction I in B in order:
-//      Gen(B) = Gen(B) U OutputRegister(I)
-//
-//   Kill(B) = {}  -- could extend this to handle DEL_FAST
-//
-// Dataflow information is propagated using the following equations:
-//
-// For definite assignment:
-//   In(B) = And(Out(P) for P in Preds(B))
-//
-// For maybe assignment:
-//   In(B) = Or(Out(P) for P in Preds(B))
-//
-// In both cases:
-//   Out(B) = Gen(B) U (In(B) - Kill(B))
-//
 } // namespace jit::hir
